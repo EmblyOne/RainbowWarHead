5027c3a7c59e9fa0d7a7c071de87fe460d75116b
Frezze <69349110+Unknown000Frezze@users.noreply.github.com>
2021-12-11 01:07:00 +0200

    Plug

---
+++ RainbowWarHead/RainbowWarHead/EventHandlers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Qurre.API.Events;
+using Qurre.API.Controllers;
+using UnityEngine;
+using MEC;
+using Qurre.API;
+
+namespace RainbowWarHead
+{ 
+
+    public class EventHandlers
+    { 
+        private readonly Plugin plugin; 
+        public EventHandlers(Plugin plugin) => this.plugin = plugin; 
+        public void RainbowMethod()
+        {
+            Timing.RunCoroutine(RainbowCoroutine(), "CorName"); 
+        }
+        public void Warheadstart(AlphaStartEvent ev)
+        {
+            Timing.RunCoroutine(RainbowCoroutine(), "CorName");
+        } 
+        public IEnumerator<float> RainbowCoroutine()
+        {
+            for(; ; )
+            {
+                Qurre.API.Controllers.Lights.ChangeColor(Color.blue);
+                yield return Timing.WaitForSeconds(3f);
+                Qurre.API.Controllers.Lights.ChangeColor(Color.green);
+                yield return Timing.WaitForSeconds(3f);
+                Qurre.API.Controllers.Lights.ChangeColor(Color.yellow);
+                yield return Timing.WaitForSeconds(3f); 
+                Qurre.API.Controllers.Lights.ChangeColor(Color.magenta);
+                yield return Timing.WaitForSeconds(3f);
+                Qurre.API.Controllers.Lights.ChangeColor(Color.cyan);
+            }
+        } 
+        public void OnRoundEnd(RoundEndEvent ev)
+        {
+            Timing.KillCoroutines("CorName");
+        }
+        public void OnRoundWaiting()
+        {
+            Timing.KillCoroutines("CorName");
+        }
+        public void OnWarHeadCancel(AlphaStopEvent ev)
+        { 
+            Timing.KillCoroutines("CorName");
+            foreach (var room in Map.Rooms)
+            {
+                room.LightOverride = false;
+
+            }   
+        }      
+    }
+}
---
+++ RainbowWarHead/RainbowWarHead/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RainbowWarHead
+{
+    public class Plugin : Qurre.Plugin
+    {
+        #region override 
+        public EventHandlers EventHandlers;
+        public override int Priority { get; } = -9999999;
+        public override string Developer { get; } = "G-Man";
+        public override string Name { get; } = "RainbowWarHead";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version NeededQurreVersion { get; } = new Version(1, 10, 0);
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        #endregion
+        #region Events
+        #region RegEvents 
+        private void RegisterEvents()
+        {
+            EventHandlers = new EventHandlers(this);
+            Qurre.Events.Alpha.Starting += EventHandlers.Warheadstart;
+            Qurre.Events.Round.End += EventHandlers.OnRoundEnd;
+            Qurre.Events.Round.Waiting += EventHandlers.OnRoundWaiting;
+            Qurre.Events.Alpha.Stopping += EventHandlers.OnWarHeadCancel; 
+        }
+        private void UnregisterEvents()
+        {
+            Qurre.Events.Alpha.Starting -= EventHandlers.Warheadstart;
+            Qurre.Events.Round.End -= EventHandlers.OnRoundEnd;
+            Qurre.Events.Round.Waiting -= EventHandlers.OnRoundWaiting;
+            Qurre.Events.Alpha.Stopping -= EventHandlers.OnWarHeadCancel;
+            EventHandlers = null; 
+        }
+       
+    }
+    #endregion
+}
+#endregion
